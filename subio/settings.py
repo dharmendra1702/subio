2239baaf390b09650d0d659d3cafdbca84bfccb5
dharmendra1702 <109207034+dharmendra1702@users.noreply.github.com>
2026-02-25 08:32:54 +0530

    fix2

--- subio/settings.py
+++ subio/settings.py
@@ -84,8 +84,7 @@ if not DATABASE_URL:
 DATABASES = {
     "default": dj_database_url.parse(
         DATABASE_URL,
-        conn_max_age=600,
-        ssl_require=True,
+        conn_max_age=0,   # IMPORTANT
     )
 }
 
